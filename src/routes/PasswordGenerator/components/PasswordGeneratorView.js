eb952ceba96fbfccb2d91acbe4409af7a28f3f38
James Tease <james.tease@replgroup.com>
2017-03-08 16:20:02 +0000

    Adding password length slider

--- src/routes/PasswordGenerator/components/PasswordGeneratorView.js
+++ src/routes/PasswordGenerator/components/PasswordGeneratorView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Slider from 'components/Slider';
 
 export function getRandomInt (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -6,22 +7,45 @@ export function getRandomInt (min, max) {
 
 export default class PasswordGeneratorView extends React.Component {
   state = {
-    password: ''
+    password: '',
+    amount: 16
   }
 
   componentWillMount () {
+    this.generate();
+  }
+
+  generate = () => {
     let password = '';
 
-    for (let i = 0; i < 17; i++) {
+    for (let i = 0; i < this.state.amount + 1; i++) {
       password += String.fromCharCode(getRandomInt(32, 122));
     }
 
     this.setState({ password });
   }
 
+  handleChange = e => {
+    const val = +e.target.value;
+    this.state[e.target.name] = +val;
+    this.generate();
+  }
+
   render () {
     return (
-      <div>{ this.state.password }</div>
+      <div>
+        <span>{ this.state.password }</span>
+
+         <Slider
+          label='Length of password'
+          step='1'
+          min='8'
+          max='64'
+          value={ this.state.amount }
+          onChange={ this.handleChange } />
+
+          <button onClick={ this.generate } className='button'>Create another</button>
+      </div>
     );
   }
 }
