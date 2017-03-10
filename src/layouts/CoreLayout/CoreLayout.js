4b095a1bc29df87331c9263a9a33f61f490f34a0
James Tease <james.tease@replgroup.com>
2017-03-10 15:36:42 +0000

    Add class to root component

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Header from '../../components/Header';
+import classnames from 'classnames';
+
 import '../../styles/core.scss';
 
-export const CoreLayout = ({ children }) => (
-  <div className='container text-center'>
-    <Header />
-    <div className='core-layout__viewport'>
-      {children}
-    </div>
-  </div>
-);
+export default class CoreLayout extends React.Component {
+  static propTypes = {
+    strength: React.PropTypes.string,
+    children: React.PropTypes.element.isRequired
+  };
 
-CoreLayout.propTypes = {
-  children : React.PropTypes.element.isRequired
-};
+  render () {
+    const classes = classnames('container', {
+      [this.props.strength]: true
+    });
 
-export default CoreLayout;
+    return (
+      <div className={ classes }>
+        <Header />
+        <div className='core-layout__viewport'>
+          { this.props.children }
+        </div>
+      </div>
+    );
+  }
+}
